5f781e7ca04fc9bce4576063dc94086020cb7c0f
Diegonarducci88 <diego.narducci88@gmail.com>
2016-10-29 22:59:38 -0200

    Update exemplo1.ino

--- exemplo1.ino
+++ exemplo1.ino
@@ -1,28 +1,24 @@
 #include <sensors.h>
 
-#define timer_scan 500
-
 sensors sensor1(11,500, 3, 5000);
 sensors sensor2(12,1000, 5, 3000);
 
 void setup() {
    pinMode(8,OUTPUT);
    pinMode(9,OUTPUT);
-   Serial.begin(9600);
 }
 
 void loop() {
   sensor1.scan_sensor();
   sensor2.scan_sensor();
-
   
-  if(sensor1.sensor_is_alarmed() == true) {
+  if(sensor1.sensor_is_alarmed()) {
     digitalWrite(8, HIGH);
   }else{
     digitalWrite(8, LOW);
   }
 
-   if(sensor2.sensor_is_alarmed() == true) {
+   if(sensor2.sensor_is_alarmed()) {
     digitalWrite(9, HIGH);
   }else{
     digitalWrite(9, LOW);
