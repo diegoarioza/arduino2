9886bf6b86734b3cca740ad3647d75895806ca75
Diegonarducci88 <diego.narducci88@gmail.com>
2016-11-06 16:37:33 -0200

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -8,24 +8,49 @@ Contato: diego.ariozza@gmail.com
 
 #include "Arduino.h"
 
-class sensors
-{
+class sensors {
 	public:
-    	sensors(int pin, int time_scan_sensor, int count, int time_alarmed);
-        void scan_sensor();
-	    bool sensor_is_alarmed();
+    	sensors(int pin, int scan_interval, int count_scan, int time_alarmed, String local_sensor, bool trigger_up_or_down = false);
+		static void var_global();
+
+		void scan();
+		void start();
+		void stop();
+		void print(int print_interval);
+		void loop();
+	    bool is_alarmed();
+		bool _trigger_up_or_down;
+
+		//
+		bool loop_v = false;
+		unsigned long _loop_millis = 0;
+		
+		String local;
 	   
     private:
-		bool _alarmed;
-	    bool _is_alarmed;
+		// --- construtor
         int _pin;
-	    int _tempo;
-	    int _count;
-	    int _count_test;
-	    int _time_alarmed;
-	    int _time_scan_sensor;
-	    unsigned long _tempo_millis;
-	    unsigned long _tempo_millis_alarmed;
-	    unsigned long _tempo_millis_zero;
+		int _scan_interval;
+		int _count_scan;
+		int _time_alarmed;
+		// --- scan
+		unsigned long _tempo_millis = 0;
+	    unsigned long _tempo_millis_alarmed = 0;
+	    unsigned long _tempo_millis_zero = 0;		
+	    int _count_scan_inc;
+		bool sensor_run;
+		bool _alarmed;
+		// --- print
+		unsigned long _print_interval_millis = 0;
+};
+
+class ldr {
+	public:
+		ldr(int pin);
+		void scan_sensor();
+
+	private:
 };
 #endif
+
+
